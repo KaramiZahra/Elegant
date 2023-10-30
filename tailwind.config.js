643ef8b1ec355918c71ce839abdb30d42932f4bb
KaramiZahra <karamizahra27k@gmail.com>
2023-10-30 21:29:57 +0330

    desktop first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./*.{html,js,css}"],
   theme: {
+    screens: {
+      xl: { max: "1200px" },
+      lg: { max: "992px" },
+      md: { max: "768px" },
+      sm: { max: "576px" },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
 
