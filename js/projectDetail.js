e235a4c92eb0eb13fbcf14504f03f3fa3347eb62
KaramiZahra <karamizahra27k@gmail.com>
2023-11-13 15:44:27 +0330

    project details page

---
+++ js/projectDetail.js
@@ -0,0 +1,282 @@
+// projects database
+let projectDB = [
+  {
+    id: 1,
+    title: "TONAL SINGLE FAMILY HOME",
+    location: "Santa Clara, California",
+    architect: "Architect: Zara's Construction",
+    photography: "Photography: Margaret Austin Photo",
+    imgSrc: [
+      "./img/projects/project1/project1-1.jpeg",
+      "./img/projects/project1/project1-2.jpeg",
+      "./img/projects/project1/project1-3.jpeg",
+      "./img/projects/project1/project1-4.jpeg",
+      "./img/projects/project1/project1-5.jpeg",
+      "./img/projects/project1/project1-6.jpeg",
+      "./img/projects/project1/project1-7.jpeg",
+      "./img/projects/project1/project1-8.jpeg",
+      "./img/projects/project1/project1-9.jpeg",
+      "./img/projects/project1/project1-10.jpeg",
+      "./img/projects/project1/project1-11.jpeg",
+      "./img/projects/project1/project1-12.jpeg",
+      "./img/projects/project1/project1-13.jpeg",
+      "./img/projects/project1/project1-14.jpeg",
+      "./img/projects/project1/project1-15.jpeg",
+      "./img/projects/project1/project1-16.jpeg",
+      "./img/projects/project1/project1-17.jpeg",
+      "./img/projects/project1/project1-18.jpeg",
+      "./img/projects/project1/project1-19.jpeg",
+      "./img/projects/project1/project1-20.jpeg",
+      "./img/projects/project1/project1-21.jpeg",
+      "./img/projects/project1/project1-22.jpeg",
+      "./img/projects/project1/project1-23.jpeg",
+      "./img/projects/project1/project1-24.jpeg",
+      "./img/projects/project1/project1-25.jpeg",
+      "./img/projects/project1/project1-26.jpeg",
+    ],
+  },
+  {
+    id: 2,
+    title: "SARATOGA WHIMSY HOUSE",
+    location: "Saratoga, California",
+    architect: "Architect: A to Z Quality Construction",
+    photography: "Photography: Margaret Austin Photo",
+    imgSrc: [
+      "./img/projects/project2/project2-1.jpeg",
+      "./img/projects/project2/project2-2.jpeg",
+      "./img/projects/project2/project2-3.jpeg",
+      "./img/projects/project2/project2-4.jpeg",
+      "./img/projects/project2/project2-5.jpeg",
+      "./img/projects/project2/project2-6.jpeg",
+      "./img/projects/project2/project2-7.jpeg",
+      "./img/projects/project2/project2-8.jpeg",
+      "./img/projects/project2/project2-9.jpeg",
+      "./img/projects/project2/project2-10.jpeg",
+      "./img/projects/project2/project2-11.jpeg",
+      "./img/projects/project2/project2-12.jpeg",
+      "./img/projects/project2/project2-13.jpeg",
+      "./img/projects/project2/project2-14.jpeg",
+      "./img/projects/project2/project2-15.jpeg",
+      "./img/projects/project2/project2-16.jpeg",
+      "./img/projects/project2/project2-17.jpeg",
+      "./img/projects/project2/project2-18.jpeg",
+      "./img/projects/project2/project2-19.jpeg",
+      "./img/projects/project2/project2-20.jpeg",
+      "./img/projects/project2/project2-21.jpeg",
+      "./img/projects/project2/project2-22.jpeg",
+      "./img/projects/project2/project2-23.jpeg",
+      "./img/projects/project2/project2-24.jpeg",
+      "./img/projects/project2/project2-25.jpeg",
+      "./img/projects/project2/project2-26.jpeg",
+      "./img/projects/project2/project2-27.jpeg",
+      "./img/projects/project2/project2-28.jpeg",
+      "./img/projects/project2/project2-29.jpeg",
+      "./img/projects/project2/project2-30.jpeg",
+    ],
+  },
+  {
+    id: 3,
+    title: "POTRERO HILL HOUSE & STUDIO",
+    location: "San Francisco, California",
+    architect: "Architect: Live Architects SF",
+    photography: "Photography: Christy Q. Photo",
+    imgSrc: [
+      "./img/projects/project3/project3-1.jpeg",
+      "./img/projects/project3/project3-2.jpeg",
+      "./img/projects/project3/project3-3.jpeg",
+      "./img/projects/project3/project3-4.jpeg",
+      "./img/projects/project3/project3-5.jpeg",
+      "./img/projects/project3/project3-6.jpeg",
+      "./img/projects/project3/project3-7.jpeg",
+      "./img/projects/project3/project3-8.jpeg",
+      "./img/projects/project3/project3-9.jpeg",
+      "./img/projects/project3/project3-10.jpeg",
+      "./img/projects/project3/project3-11.jpeg",
+      "./img/projects/project3/project3-12.jpeg",
+      "./img/projects/project3/project3-13.jpeg",
+      "./img/projects/project3/project3-14.jpeg",
+      "./img/projects/project3/project3-15.jpeg",
+      "./img/projects/project3/project3-16.jpeg",
+      "./img/projects/project3/project3-17.jpeg",
+      "./img/projects/project3/project3-18.jpeg",
+      "./img/projects/project3/project3-19.jpeg",
+      "./img/projects/project3/project3-20.jpeg",
+    ],
+  },
+  {
+    id: 4,
+    title: "PALO ALTO MULTI-GENERATIONAL HOME",
+    location: "Palo Alto, California",
+    architect: "Architect: Quality Construction",
+    photography: "Photography: Christy Q. Photography",
+    imgSrc: [
+      "./img/projects/project4/project4-1.jpeg",
+      "./img/projects/project4/project4-2.jpeg",
+      "./img/projects/project4/project4-3.jpeg",
+      "./img/projects/project4/project4-4.jpeg",
+      "./img/projects/project4/project4-5.jpeg",
+      "./img/projects/project4/project4-6.jpeg",
+      "./img/projects/project4/project4-7.jpeg",
+      "./img/projects/project4/project4-8.jpeg",
+      "./img/projects/project4/project4-9.jpeg",
+      "./img/projects/project4/project4-10.jpeg",
+      "./img/projects/project4/project4-11.jpeg",
+      "./img/projects/project4/project4-12.jpeg",
+      "./img/projects/project4/project4-13.jpeg",
+      "./img/projects/project4/project4-14.jpeg",
+      "./img/projects/project4/project4-15.jpeg",
+      "./img/projects/project4/project4-16.jpeg",
+      "./img/projects/project4/project4-17.jpeg",
+      "./img/projects/project4/project4-18.jpeg",
+      "./img/projects/project4/project4-19.jpeg",
+      "./img/projects/project4/project4-20.jpeg",
+    ],
+  },
+  {
+    id: 5,
+    title: "CAMPBELL MODERN HOME",
+    location: " Los Angeles, California",
+    architect: "Architect: Zara's Construction",
+    photography: "Photography: Margaret Austin Photo",
+    imgSrc: [
+      "./img/projects/project5/project5-1.jpeg",
+      "./img/projects/project5/project5-2.jpeg",
+      "./img/projects/project5/project5-3.jpeg",
+      "./img/projects/project5/project5-4.jpeg",
+      "./img/projects/project5/project5-5.jpeg",
+      "./img/projects/project5/project5-6.jpeg",
+      "./img/projects/project5/project5-7.jpeg",
+      "./img/projects/project5/project5-8.jpeg",
+      "./img/projects/project5/project5-9.jpeg",
+      "./img/projects/project5/project5-10.jpeg",
+      "./img/projects/project5/project5-11.jpeg",
+      "./img/projects/project5/project5-12.jpeg",
+      "./img/projects/project5/project5-13.jpeg",
+      "./img/projects/project5/project5-14.jpeg",
+      "./img/projects/project5/project5-15.jpeg",
+      "./img/projects/project5/project5-16.jpeg",
+      "./img/projects/project5/project5-17.jpeg",
+    ],
+  },
+  {
+    id: 6,
+    title: "PACIFIC NORTHWEST PRIMARY",
+    location: "Newcastle, Washington",
+    architect: "Architect: A to Z Quality Construction",
+    photography: "Photography: Talitha Photos",
+    imgSrc: [
+      "./img/projects/project6/project6-1.jpeg",
+      "./img/projects/project6/project6-2.jpeg",
+      "./img/projects/project6/project6-3.jpeg",
+      "./img/projects/project6/project6-4.jpeg",
+      "./img/projects/project6/project6-5.jpeg",
+      "./img/projects/project6/project6-6.jpeg",
+      "./img/projects/project6/project6-7.jpeg",
+      "./img/projects/project6/project6-8.jpeg",
+      "./img/projects/project6/project6-9.jpeg",
+    ],
+  },
+  {
+    id: 7,
+    title: "EARTHY MACKAY HOUSE",
+    location: "Mountain View, California",
+    architect: "Architect: Anonymous Construction",
+    photography: "Photography: Margaret Austin Photo",
+    imgSrc: [
+      "./img/projects/project7/project7-1.jpeg",
+      "./img/projects/project7/project7-2.jpeg",
+      "./img/projects/project7/project7-3.jpeg",
+      "./img/projects/project7/project7-4.jpeg",
+      "./img/projects/project7/project7-5.jpeg",
+      "./img/projects/project7/project7-6.jpeg",
+      "./img/projects/project7/project7-7.jpeg",
+      "./img/projects/project7/project7-8.jpeg",
+      "./img/projects/project7/project7-9.jpeg",
+      "./img/projects/project7/project7-10.jpeg",
+      "./img/projects/project7/project7-11.jpeg",
+      "./img/projects/project7/project7-12.jpeg",
+      "./img/projects/project7/project7-13.jpeg",
+      "./img/projects/project7/project7-14.jpeg",
+      "./img/projects/project7/project7-15.jpeg",
+      "./img/projects/project7/project7-16.jpeg",
+      "./img/projects/project7/project7-17.jpeg",
+      "./img/projects/project7/project7-18.jpeg",
+    ],
+  },
+  {
+    id: 8,
+    title: "AUSTRALIAN MINIMALIST GROUND UP",
+    location: "Campbell, California",
+    architect: "Architect: Raumfabrik Architecture",
+    photography: "Photography: Margaret Austin Photo",
+    imgSrc: [
+      "./img/projects/project8/project8-1.jpeg",
+      "./img/projects/project8/project8-2.jpeg",
+      "./img/projects/project8/project8-3.jpeg",
+      "./img/projects/project8/project8-4.jpeg",
+      "./img/projects/project8/project8-5.jpeg",
+      "./img/projects/project8/project8-6.jpeg",
+      "./img/projects/project8/project8-7.jpeg",
+      "./img/projects/project8/project8-8.jpeg",
+      "./img/projects/project8/project8-9.jpeg",
+      "./img/projects/project8/project8-10.jpeg",
+      "./img/projects/project8/project8-11.jpeg",
+      "./img/projects/project8/project8-12.jpeg",
+      "./img/projects/project8/project8-13.jpeg",
+      "./img/projects/project8/project8-14.jpeg",
+      "./img/projects/project8/project8-15.jpeg",
+      "./img/projects/project8/project8-16.jpeg",
+      "./img/projects/project8/project8-17.jpeg",
+      "./img/projects/project8/project8-18.jpeg",
+      "./img/projects/project8/project8-19.jpeg",
+      "./img/projects/project8/project8-20.jpeg",
+      "./img/projects/project8/project8-21.jpeg",
+      "./img/projects/project8/project8-22.jpeg",
+      "./img/projects/project8/project8-23.jpeg",
+      "./img/projects/project8/project8-24.jpeg",
+      "./img/projects/project8/project8-25.jpeg",
+      "./img/projects/project8/project8-26.jpeg",
+      "./img/projects/project8/project8-27.jpeg",
+      "./img/projects/project8/project8-28.jpeg",
+      "./img/projects/project8/project8-29.jpeg",
+      "./img/projects/project8/project8-30.jpeg",
+    ],
+  },
+];
+
+// create "A SINGLE" project and insert it into DOM
+let header = document.querySelector(".header");
+let projectContainer = document.querySelector(".project-container");
+function createProjects(project) {
+  // insert data into header
+  header.insertAdjacentHTML(
+    "beforeend",
+    '<h1 class="title">' +
+      project.title +
+      "</h1><p>" +
+      project.location +
+      "</p><p>" +
+      project.architect +
+      "</p><p>" +
+      project.photography +
+      "</p>"
+  );
+
+  // Insert project images into projectContainer
+  project.imgSrc.forEach((src) => {
+    projectContainer.insertAdjacentHTML("beforeend", '<img src="' + src + '">');
+  });
+}
+
+// retrieve a specific item's id from the current page's URL
+let locationParams = new URLSearchParams(location.search);
+let itemID = locationParams.get("id");
+
+// check if there is an item with the given id and create the respective page
+let findItem = projectDB.find((item) => {
+  return item.id == itemID;
+});
+// console.log(findItem);
+if (findItem) {
+  createProjects(findItem);
+}
--- js/projects.js
+++ js/projects.js
@@ -3,42 +3,42 @@ let projectsDB = [
   {
     id: 1,
     img: "./img/projects/project1/project1-10.jpeg",
-    caption: "Project 1",
+    caption: "TONAL SINGLE FAMILY HOME",
   },
   {
     id: 2,
     img: "./img/projects/project2/project2-14.jpeg",
-    caption: "Project 2",
+    caption: "SARATOGA WHIMSY HOUSE",
   },
   {
     id: 3,
     img: "./img/projects/project3/project3-10.jpeg",
-    caption: "Project 3",
+    caption: "POTRERO HILL HOUSE & STUDIO",
   },
   {
     id: 4,
     img: "./img/projects/project4/project4-4.jpeg",
-    caption: "Project 4",
+    caption: "PALO ALTO MULTI-GENERATIONAL HOME",
   },
   {
     id: 5,
     img: "./img/projects/project5/project5-14.jpeg",
-    caption: "Project 5",
+    caption: "CAMPBELL MODERN HOME",
   },
   {
     id: 6,
     img: "./img/projects/project6/project6-4.jpeg",
-    caption: "Project 6",
+    caption: "PACIFIC NORTHWEST PRIMARY",
   },
   {
     id: 7,
     img: "./img/projects/project7/project7-9.jpeg",
-    caption: "Project 7",
+    caption: "EARTHY MACKAY HOUSE",
   },
   {
     id: 8,
     img: "./img/projects/project8/project8-4.jpeg",
-    caption: "Project 8",
+    caption: "AUSTRALIAN MINIMALIST GROUND UP",
   },
 ];
 let projectsContainer = document.querySelector(".projects-container");
