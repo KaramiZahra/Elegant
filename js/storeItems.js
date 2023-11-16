884734d1fcd4716374fde64a0a9a50c0dc10d305
KaramiZahra <karamizahra27k@gmail.com>
2023-11-16 18:46:42 +0330

    store items page

---
+++ js/storeItems.js
@@ -0,0 +1,247 @@
+let itemsDB = [
+  {
+    id: 1,
+    title: "Bed",
+    itemName: [
+      "Bed1",
+      "Bed2",
+      "Bed3",
+      "Bed4",
+      "Bed5",
+      "Bed6",
+      "Bed7",
+      "Bed8",
+      "Bed9",
+      "Bed10",
+    ],
+    itemPrice: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+    imgSrc: [
+      "./img/store/bed/bed1.jpeg",
+      "./img/store/bed/bed2.jpeg",
+      "./img/store/bed/bed3.jpeg",
+      "./img/store/bed/bed4.jpeg",
+      "./img/store/bed/bed5.jpeg",
+      "./img/store/bed/bed6.jpeg",
+      "./img/store/bed/bed7.jpeg",
+      "./img/store/bed/bed8.jpeg",
+      "./img/store/bed/bed9.jpeg",
+      "./img/store/bed/bed10.jpeg",
+    ],
+  },
+  {
+    id: 2,
+    title: "Chair",
+    itemName: [
+      "Chair1",
+      "Chair2",
+      "Chair3",
+      "Chair4",
+      "Chair5",
+      "Chair6",
+      "Chair7",
+      "Chair8",
+      "Chair9",
+      "Chair10",
+    ],
+    itemPrice: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+    imgSrc: [
+      "./img/store/chair/chair1.jpeg",
+      "./img/store/chair/chair2.jpeg",
+      "./img/store/chair/chair3.jpeg",
+      "./img/store/chair/chair4.jpeg",
+      "./img/store/chair/chair5.jpeg",
+      "./img/store/chair/chair6.jpeg",
+      "./img/store/chair/chair7.jpeg",
+      "./img/store/chair/chair8.jpeg",
+      "./img/store/chair/chair9.jpeg",
+      "./img/store/chair/chair10.jpeg",
+    ],
+  },
+  {
+    id: 3,
+    title: "Decor",
+    itemName: [
+      "Decor1",
+      "Decor2",
+      "Decor3",
+      "Decor4",
+      "Decor5",
+      "Decor6",
+      "Decor7",
+      "Decor8",
+      "Decor9",
+      "Decor10",
+      "Decor11",
+      "Decor12",
+      "Decor13",
+      "Decor14",
+      "Decor15",
+    ],
+    itemPrice: [
+      100, 110, 200, 210, 300, 310, 400, 410, 500, 510, 600, 700, 800, 900,
+      1000,
+    ],
+    imgSrc: [
+      "./img/store/decor/decor1.jpeg",
+      "./img/store/decor/decor2.jpeg",
+      "./img/store/decor/decor3.jpeg",
+      "./img/store/decor/decor4.jpeg",
+      "./img/store/decor/decor5.jpeg",
+      "./img/store/decor/decor6.jpeg",
+      "./img/store/decor/decor7.jpeg",
+      "./img/store/decor/decor8.jpeg",
+      "./img/store/decor/decor9.jpeg",
+      "./img/store/decor/decor10.jpeg",
+      "./img/store/decor/decor11.jpeg",
+      "./img/store/decor/decor12.jpeg",
+      "./img/store/decor/decor13.webp",
+      "./img/store/decor/decor14.jpeg",
+      "./img/store/decor/decor15.webp",
+    ],
+  },
+  {
+    id: 4,
+    title: "Lamp",
+    itemName: [
+      "Lamp1",
+      "Lamp2",
+      "Lamp3",
+      "Lamp4",
+      "Lamp5",
+      "Lamp6",
+      "Lamp7",
+      "Lamp8",
+      "Lamp9",
+      "Lamp10",
+      "Lamp11",
+      "Lamp12",
+      "Lamp13",
+      "Lamp14",
+      "Lamp15",
+      "Lamp16",
+    ],
+    itemPrice: [
+      100, 110, 200, 210, 300, 310, 400, 410, 500, 510, 600, 610, 700, 800, 900,
+      1000,
+    ],
+    imgSrc: [
+      "./img/store/lamp/lamp1.jpeg",
+      "./img/store/lamp/lamp2.jpeg",
+      "./img/store/lamp/lamp3.jpeg",
+      "./img/store/lamp/lamp4.jpeg",
+      "./img/store/lamp/lamp5.jpeg",
+      "./img/store/lamp/lamp6.jpeg",
+      "./img/store/lamp/lamp7.jpeg",
+      "./img/store/lamp/lamp8.jpeg",
+      "./img/store/lamp/lamp9.jpeg",
+      "./img/store/lamp/lamp10.jpeg",
+      "./img/store/lamp/lamp11.jpeg",
+      "./img/store/lamp/lamp12.jpeg",
+      "./img/store/lamp/lamp13.jpeg",
+      "./img/store/lamp/lamp14.jpeg",
+      "./img/store/lamp/lamp15.jpeg",
+      "./img/store/lamp/lamp16.jpeg",
+    ],
+  },
+  {
+    id: 5,
+    title: "Sofa",
+    itemName: [
+      "Sofa1",
+      "Sofa2",
+      "Sofa3",
+      "Sofa4",
+      "Sofa5",
+      "Sofa6",
+      "Sofa7",
+      "Sofa8",
+      "Sofa9",
+      "Sofa10",
+      "Sofa11",
+      "Sofa12",
+      "Sofa13",
+    ],
+    itemPrice: [
+      100, 110, 200, 210, 300, 310, 400, 500, 600, 700, 800, 900, 1000,
+    ],
+    imgSrc: [
+      "./img/store/sofa/sofa1.jpeg",
+      "./img/store/sofa/sofa2.jpeg",
+      "./img/store/sofa/sofa3.jpeg",
+      "./img/store/sofa/sofa4.jpeg",
+      "./img/store/sofa/sofa5.jpeg",
+      "./img/store/sofa/sofa6.jpeg",
+      "./img/store/sofa/sofa7.jpeg",
+      "./img/store/sofa/sofa8.jpeg",
+      "./img/store/sofa/sofa9.jpeg",
+      "./img/store/sofa/sofa10.jpeg",
+      "./img/store/sofa/sofa11.jpeg",
+      "./img/store/sofa/sofa12.jpeg",
+      "./img/store/sofa/sofa13.jpeg",
+    ],
+  },
+  {
+    id: 6,
+    title: "Table",
+    itemName: [
+      "Table1",
+      "Table2",
+      "Table3",
+      "Table4",
+      "Table5",
+      "Table6",
+      "Table7",
+      "Table8",
+    ],
+    itemPrice: [100, 200, 300, 400, 500, 600, 700, 800],
+    imgSrc: [
+      "./img/store/table/table1.jpeg",
+      "./img/store/table/table2.jpeg",
+      "./img/store/table/table3.jpeg",
+      "./img/store/table/table4.jpeg",
+      "./img/store/table/table5.jpeg",
+      "./img/store/table/table6.jpeg",
+      "./img/store/table/table7.jpeg",
+      "./img/store/table/table8.jpeg",
+    ],
+  },
+];
+
+// get the page id
+let locationParams = new URLSearchParams(location.search);
+let getID = locationParams.get("id");
+
+// find the respective id's data
+let findItem = itemsDB.find((item) => {
+  return item.id == getID;
+});
+
+// create items for the given id
+let itemContainer = document.querySelector(".item-container");
+let header = document.querySelector("#header");
+
+function createItem(item) {
+  // insert header title
+  header.insertAdjacentHTML(
+    "beforeend",
+    '<h1 class="title">' + item.title + '</h1><div class="underline"></div>'
+  );
+
+  // Iterate through the arrays for the current item
+  for (let i = 0; i < item.itemName.length; i++) {
+    itemContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      <a href="#" class="item" id="${[i + 1]}">
+        <img src="${item.imgSrc[i]}">
+        <div class="overlay">View Details</div>
+        <div class="item-desc">
+          <p>${item.itemName[i]}</p>
+          <p>${item.itemPrice[i]}$</p>
+        </div>
+      </a>
+    `
+    );
+  }
+}
+createItem(findItem);
